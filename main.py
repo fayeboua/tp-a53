1d51a40374ac575076833aaa05cbf71394517955
Geoffroy AYEBOUA <geoffroy.ayeboua@gmail.com>
2024-06-19 08:16:14 -0400

    attempt to run scheduler on github action

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import schedule
 from service.scrappy import job
 
 if __name__ == '__main__':
-    schedule.every(3).hours.do(job)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # schedule.every(3).hours.do(job)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+    job()
